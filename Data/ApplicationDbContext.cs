bbcb50e9e7600cc2db464c754721c586538bf2c0
jasogwa <jasogwa@nanocosmos.net>
2024-07-02 13:29:07 +0200

    data

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantFeedbackSystem.Models;
+
+namespace RestaurantFeedbackSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<FeedbackResponse> FeedbackResponses { get; set; } 
+
+    }
+}
