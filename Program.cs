6dff60cd7453df4f1f820897429ea51510762d55
jasogwa <jasogwa@nanocosmos.net>
2024-07-02 13:28:43 +0200

    program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using RestaurantFeedbackSystem.Data;
+using RestaurantFeedbackSystem.Interfaces;
+using RestaurantFeedbackSystem.Repositories;
+using RestaurantFeedbackSystem.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantFeedbackSystemApiConnectionString")));
+
+builder.Services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+builder.Services.AddScoped<IFeedbackResponseRepository, FeedbackResponseRepository>();
+builder.Services.AddScoped<IRestaurantService, RestaurantService>();
+builder.Services.AddScoped<IFeedbackService, FeedbackService>();
+builder.Services.AddScoped<IFeedbackResponseService, FeedbackResponseService>();
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,6 +27,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
