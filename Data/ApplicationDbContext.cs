b7116860b142ed340c312600fcec92d8ed94f8a3
jasogwa <jasogwa@nanocosmos.net>
2024-07-04 10:53:19 +0200

    model override

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,7 +9,19 @@ namespace RestaurantFeedbackSystem.Data
 
         public DbSet<Restaurant> Restaurants { get; set; }
         public DbSet<Feedback> Feedbacks { get; set; }
-        public DbSet<FeedbackResponse> FeedbackResponses { get; set; } 
+        public DbSet<FeedbackResponse> FeedbackResponses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(r => r.Feedbacks)
+                .WithOne(f => f.Restaurant)
+                .HasForeignKey(f => f.RestaurantId);
+
+            modelBuilder.Entity<Feedback>()
+                .HasOne(f => f.Response)
+                .WithOne(r => r.Feedback)
+                .HasForeignKey<FeedbackResponse>(r => r.FeedbackId);
+        }
     }
 }
